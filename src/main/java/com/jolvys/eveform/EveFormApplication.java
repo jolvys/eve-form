b51b0c0ef450a0603c8e82f38df422ee242ae162
jolvy <jolvy@gmail.com>
2025-01-02 22:52:53 +0900

    initial commit

---
+++ src/main/java/com/jolvys/eveform/EveFormApplication.java
@@ -0,0 +1,13 @@
+package com.jolvys.eveform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jolvys/eveform/EveFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jolvys.eveform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
